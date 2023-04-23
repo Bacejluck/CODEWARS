7d1ebb50b44ad162e73c2b94b5a6c59427c7a7ba
Bacejluck <119012783+Bacejluck@users.noreply.github.com>
2023-04-23 23:25:45 +0300

    Update aAbBcC.js
    преобразует элементы списка так что первый элемент каждой страки имеет верхний регистр ,ДОБАВИЛ КОМЕНТ

--- aAbBcC.js
+++ aAbBcC.js
@@ -2,9 +2,7 @@ let names=["qww",'fgsgfsd','fdgdfg','fffg','fdsfadf','dsfsadf'];
 let newNames= [];
 for (let i = 0;i <names.length;i++){
     let name = names[i];
-    let firstLetter = name.charAt(0).toUpperCase();
-    console.log(firstLetter)
-    newNames.push(firstLetter+name.slice(1));
-    console.log(name.slice(1))
-}
+    let firstLetter = name.charAt(0).toUpperCase();//charAT берет элемент строки по индексу toUpperCase преобразхует в верхний регистр
+    newNames.push(firstLetter+name.slice(1));//push добавляет элемент в спикос slice копирует от индекса до конеца (если конец не указан)
+}   
 console.log(newNames);
